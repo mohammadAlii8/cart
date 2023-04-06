d8409bc0df7d4ada1d214f5f2f50a48fecd46073
MohammadAli <galaxy.mrz8@gmail.com>
2023-04-06 23:43:26 +0430

    quantityCount handler and Trash Icon added to prj

--- src/Components/Shared/Product.js
+++ src/Components/Shared/Product.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom"
 
 //  Function
-import { shorten } from '../../helper/functions';
+import { shorten, isInCart, quantityCount } from '../../helper/functions';
 
+// CONTEXT
+import { cartContext } from '../../Context/CartContextProvider';
+
+// ICONS
+import trashIcon from "../../assets/icons/trash.svg"
 const Product = ({ productData }) => {
+
+    const { state, dispatch } = useContext(cartContext)
+
     return (
         <div>
             <img src={productData.image} alt={productData.title} style={{ width: "200px" }} />
             <h3>{shorten(productData.title)}</h3>
             <p>{productData.price}</p>
             <div>
-                <Link to={`/products/${productData.id}`}>Details</Link>
-                <button>Add to cart</button>
+                <Link to={`/products/${ productData.id }`}>Details</Link>
+                <div>
+                    {
+                        quantityCount(state, productData.id) > 1 && <button onClick={() => dispatch({ type: "DECREASE", payload: productData })}>-</button>
+                    }
+                    {
+                        quantityCount(state, productData.id) === 1 && <button onClick={() => dispatch({ type: "REMOVE_ITEM", payload: productData })}>
+                            <img src={trashIcon} alt="Trash" style={{ width: "20px" }} />
+                        </button>
+                    }
+                    {
+                        isInCart(state, productData.id) ? <button onClick={() => dispatch({ type: "INCREASE", payload: productData })}>+</button> :
+                            <button onClick={() => dispatch({ type: "ADD_ITEM", payload: productData })}>Add To cart</button>
+                    }
+
+                </div>
             </div>
         </div>
     );
